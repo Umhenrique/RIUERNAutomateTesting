cb78357bc415007f0ef68f06d66a1737bfc3b635
Umhenrique <Umhenrique17@gmail.com>
2025-10-27 12:44:28 -0300

    auto email

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,9 +1,23 @@
 describe('template spec', () => {
-  it('passes', () => {
+  it('Creating account by email', () => {
     cy.visit('https://repositorio.apps.uern.br/')
     cy.get('[data-test="login-menu"]').click();
     cy.get('[data-test="register"]').click();
-    cy.get('#email').type('test@example.com');
+    cy.wait(1000);
+    cy.mailslurp()
+      .then(mailslurp => mailslurp.createInbox())
+      .then(inbox => {
+        cy.wrap(inbox.id).as('inboxId');
+        cy.wrap(inbox.emailAddress).as('emailAddress');
+      });
+    cy.then(function () {
+      expect(this.emailAddress).to.exist;
+      cy.get('#email').type(this.emailAddress);
+    });
     cy.get('#main-content button.ng-star-inserted').click();
-  })
-})
+  });
+
+  it("verify email received", () => {
+    // Add verification steps here
+  });
+});
